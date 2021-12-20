1fc9b243e85bc088d895d1cedf6fd6bfd0c0144c
Oleksii Kachaiev <okachaiev@riotgames.com>
2021-12-19 16:55:42 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# Gym-μRTS with Stable-Baselines3
+
+This repo contains an attempt to reproduce Gridnet PPO with invalid action masking algorithm to play μRTS using [stable-baselines3](https://github.com/DLR-RM/stable-baselines3) library. Apart from reproducibility, this might open access to a diverse set of well tested algorithms, and toolings for training, evaluations, and more.
+
+Original paper: [Gym-μRTS: Toward Affordable Deep Reinforcement Learning Research in Real-time Strategy Games](https://arxiv.org/abs/2105.13807).
+
+Original code: [gym-microrts-paper](https://github.com/vwxyzjn/gym-microrts-paper).
+
+## Install
+
+Make sure you have `jdk`>=1.8.0 installed. Install Python dependencies:
+
+```
+git clone https://github.com/kachayev/gym-microrts-paper-sb3
+cd gym-microrts-paper-sb3
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+Most dependencies are copied from the original code [here](https://github.com/vwxyzjn/gym-microrts-paper/blob/master/requirements.txt), with a few caveats. Original versions are commented out in the file, if you are curious.
+
+Video capturing requires `ffmpeg` as well.
+
+## Training
+
+To traing an agent, please run
+
+```
+$ python ppo_gridnet_diverse_encode_decode_sb3.py
+```
+
+If everything is setup correctly, you'll see typicall Stable-Baselines3 verbose logging:
+
+```
+Using cpu device
+---------------------------------
+| rollout/           |          |
+|    ep_len_mean     | 2e+03    |
+|    ep_rew_mean     | 0.0      |
+| time/              |          |
+|    fps             | 179      |
+|    iterations      | 1        |
+|    time_elapsed    | 11       |
+|    total_timesteps | 2048     |
+---------------------------------
+------------------------------------------
+| rollout/                |              |
+|    ep_len_mean          | 1.72e+03     |
+|    ep_rew_mean          | -5.0         |
+| time/                   |              |
+|    fps                  | 55           |
+|    iterations           | 2            |
+|    time_elapsed         | 74           |
+|    total_timesteps      | 4096         |
+| train/                  |              |
+|    approx_kl            | 0.0056759235 |
+|    clip_fraction        | 0.0861       |
+|    clip_range           | 0.2          |
+|    entropy_loss         | -5.65        |
+|    explained_variance   | 0.412        |
+|    learning_rate        | 0.0003       |
+|    loss                 | -0.024       |
+|    n_updates            | 10           |
+|    policy_gradient_loss | -0.00451     |
+|    value_loss           | 0.00413      |
+------------------------------------------
+```
+
+As soon as correctness of the implementation is verified, I will provide details on how to use RL Baselines3 Zoo for training and evaluations.
+
+## Implementational Caveats
+
+A few notes / pain points regarding the implementation of the alrogithms, and the process of integrating it with stable-baselines3:
+
+* Action spaces for the environment is specified as `gym.spaces.MultiDiscrete`, [here](https://github.com/vwxyzjn/gym-microrts/blob/34c0e567c37eaae85159207986a851b702bf427e/gym_microrts/envs/vec_env.py#L104). This is not exectly correct, action space for gridnet is an array of `MultiDiscrete` actions (see the paper for further explanations). In a way, this misleads SB3 in many different places.
+* Gym does not ship a space for "array of multidiscrete" use case (let's be honest, it's not very common). But it gives an option for defining your space when necessary. A new space, when defined, is not easy to integrate into SB3. In a few different places SB3 raises `NotImplementedError` facing unknown space ([example 1](https://github.com/DLR-RM/stable-baselines3/blob/df6f9de8f46509dad47e6d2e5620aa993b0fc883/stable_baselines3/common/distributions.py#L644), [example 2](https://github.com/DLR-RM/stable-baselines3/blob/df6f9de8f46509dad47e6d2e5620aa993b0fc883/stable_baselines3/common/preprocessing.py#L183)).
+* `RolloutBuffer` for on-policy algorithms cannot be replaced with own implementation. And existing implementation from the library does not support non-flat actions. E.g. here we have action as a (256, 7) tensor. Those do not fit into the buffer. To avoid the problem, policy methods are modified to flatten actions, `action_dims` dynamically injected to represent the flattened layout, the environment is modified to reshape actions before executing `step`.
+* Invalid masking is implemented by passing masks into observations from the wrapper (the observation space is replaced with `gym.spaces.Dict` to hold both observations and masks). By doing it this way, masks are now available for policy, and fit rollout buffer layout. Masking is implemented by setting logits into `-inf` (or to a rather small number).
---
+++ ppo_gridnet_diverse_encode_decode_sb3.py
@@ -0,0 +1,265 @@
+import numpy as np
+import time
+import torch
+from torch import nn
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.distributions import MultiCategoricalDistribution
+from stable_baselines3.common.policies import ActorCriticPolicy, register_policy
+from stable_baselines3.common.torch_layers import MlpExtractor
+from stable_baselines3.common.utils import get_device
+from stable_baselines3.common.vec_env import VecEnvWrapper
+
+import gym
+import gym_microrts
+from gym_microrts.envs.vec_env import MicroRTSGridModeVecEnv
+from gym_microrts import microrts_ai
+
+# code from here:
+# https://github.com/vwxyzjn/gym-microrts-paper/blob/cf291b303c04e98be2f00acbbe6bbb2c23a8bac5/ppo_gridnet_diverse_encode_decode.py#L96
+class VecMonitor(VecEnvWrapper):
+    def __init__(self, venv):
+        VecEnvWrapper.__init__(self, venv)
+        self.eprets = None
+        self.eplens = None
+        self.epcount = 0
+        self.tstart = time.time()
+
+    def reset(self):
+        obs = self.venv.reset()
+        self.eprets = np.zeros(self.num_envs, 'f')
+        self.eplens = np.zeros(self.num_envs, 'i')
+        return obs
+
+    def step_wait(self):
+        obs, rews, dones, infos = self.venv.step_wait()
+        self.eprets += rews
+        self.eplens += 1
+
+        newinfos = list(infos[:])
+        for i in range(len(dones)):
+            if dones[i]:
+                info = infos[i].copy()
+                ret = self.eprets[i]
+                eplen = self.eplens[i]
+                epinfo = {'r': ret, 'l': eplen, 't': round(time.time() - self.tstart, 6)}
+                info['episode'] = epinfo
+                self.epcount += 1
+                self.eprets[i] = 0
+                self.eplens[i] = 0
+                newinfos[i] = info
+        return obs, rews, dones, newinfos
+
+
+class CustomMicroRTSGridMode(MicroRTSGridModeVecEnv):
+
+    def __init__(self, *args, **kwargs):
+        # xxx(okachaiev): seems like there's no need to provide
+        # this parameter separately
+        kwargs['num_bot_envs'] = len(kwargs.get('ai2s', []))
+        super().__init__(*args, **kwargs)
+        self.observation_space = gym.spaces.Dict({
+            "obs": self.observation_space,
+            "masks": gym.spaces.Box(
+                low=0.0,
+                high=1.0,
+                shape=(self.height*self.width, self.action_space.nvec[1:].sum()),
+                dtype=np.int32
+            ),
+        })
+
+    def get_action_mask(self):
+        action_mask = np.array(self.vec_client.getMasks(0))
+        action_type_and_parameter_mask = action_mask[:,:,:,1:].reshape(
+            self.num_envs,
+            self.height*self.width,
+            -1
+        )
+        return action_type_and_parameter_mask
+
+    def step_async(self, action):
+        action = action.reshape(self.num_envs, self.height*self.width, len(self.action_space.nvec[1:]))
+        return super().step_async(action)
+
+    def step(self, action):
+        action = action.reshape(self.num_envs, self.height*self.width, len(self.action_space.nvec[1:]))
+        return super().step(action)
+
+    def step_wait(self):
+        obs, rewards, dones, infos = super().step_wait()
+        masks = self.get_action_mask()
+        return {"obs": obs, "masks": masks}, rewards, dones, infos
+
+    def reset(self):
+        obs = super().reset()
+        masks = self.get_action_mask()
+        return {"obs": obs, "masks": masks}
+
+
+class Transpose(nn.Module):
+    def __init__(self, permutation):
+        super().__init__()
+        self.permutation = permutation
+
+    def forward(self, x):
+        return x.permute(self.permutation)
+
+
+def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
+    torch.nn.init.orthogonal_(layer.weight, std)
+    torch.nn.init.constant_(layer.bias, bias_const)
+    return layer
+
+# xxx(okachaiev): should this go to "feature extractor" configuration?
+class MicroRTSExtractor(MlpExtractor):
+
+    def __init__(self, input_channels=27, output_channels=78, device = "auto"):
+        super(MlpExtractor, self).__init__()
+
+        # xxx(okachaiev): requires reading the documentation
+        # to know about these properties. maybe ABC class
+        # with proper exception if the method is not implemented
+        # would be cleaner?
+        self.latent_dim_pi = output_channels
+        self.latent_dim_vf = 1
+    
+        device = get_device(device)
+
+        self.shared_net = nn.Sequential(
+            Transpose((0, 3, 1, 2)),
+            layer_init(nn.Conv2d(input_channels, 32, kernel_size=3, padding=1)),
+            nn.MaxPool2d(3, stride=2, padding=1),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(32, 64, kernel_size=3, padding=1)),
+            nn.MaxPool2d(3, stride=2, padding=1),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(64, 128, kernel_size=3, padding=1)),
+            nn.MaxPool2d(3, stride=2, padding=1),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(128, 256, kernel_size=3, padding=1)),
+            nn.MaxPool2d(3, stride=2, padding=1)
+        ).to(device)
+
+        self.policy_net = nn.Sequential(
+            layer_init(nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1)),
+            nn.ReLU(),
+            layer_init(nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1)),
+            nn.ReLU(),
+            layer_init(nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1)),
+            nn.ReLU(),
+            layer_init(nn.ConvTranspose2d(32, output_channels, 3, stride=2, padding=1, output_padding=1)),
+            Transpose((0, 2, 3, 1)),
+        ).to(device)
+
+        self.value_net = nn.Sequential(
+            nn.Flatten(),
+            layer_init(nn.Linear(256, 128), std=1),
+            nn.ReLU(),
+            layer_init(nn.Linear(128, 1), std=1),
+        ).to(device)
+
+
+class MicroRTSGridActorCritic(ActorCriticPolicy):
+
+    # xxx(okachaiev): hack
+    # seems like redefining "internal" method is not
+    # a good idea
+    def _build(self, lr_schedule) -> None:
+        self._build_mlp_extractor()
+        # xxx(okachaiev): hack
+        # not sure if turning nets & extractors into identity
+        # layers is how it supposed to be done
+        self.action_net = nn.Identity()
+        self.value_net = nn.Identity()
+        self.features_extractor = nn.Identity()
+        self.optimizer = self.optimizer_class(self.parameters(), lr=lr_schedule(1), **self.optimizer_kwargs)
+
+        # xxx(okachaiev): hack to make multicategorical work
+        # we need to ignore the first dimention of 256 grid cells
+        self.action_dist = MultiCategoricalDistribution(self.action_space.nvec[1:])
+
+    def _build_mlp_extractor(self) -> None:
+        self.mlp_extractor = MicroRTSExtractor(
+            input_channels=27,
+            output_channels=self.action_space.nvec[1:].sum()
+        )
+
+    def extract_features(self, obs):
+        return obs['obs'].float()
+
+    def forward(self, obs, deterministic: bool = False):
+        features = self.extract_features(obs)
+        latent_pi, latent_vf = self.mlp_extractor(features)
+        values = self.value_net(latent_vf)
+
+        # xxx(okachaiev): hack to make multicategorical work
+        # (1, 16, 16, 78) -> (1*16*16, 78)
+        # logits for each position of each environment
+        masks = obs['masks'].reshape(-1, self.action_space.nvec[1:].sum())
+        latent_pi = latent_pi.reshape(-1, self.action_space.nvec[1:].sum())
+        latent_pi = torch.where(masks.bool(), latent_pi, torch.tensor(-1e+8, device=self.device))
+
+        # 1*16*16 distributions for each position on each environment
+        distribution = self._get_action_dist_from_latent(latent_pi)
+        actions = distribution.get_actions(deterministic=deterministic)
+        log_prob = distribution.log_prob(actions)
+
+        # xxx(okachaiev): hack
+        # it should be (n_envs, w*h, n_categorical) = (1, 16*16, 7)
+        # but i'm compressing it into (1, 16*16*7) so sb3 can put
+        # it into rollout buffer
+        actions = actions.reshape(obs['obs'].shape[0], -1)
+
+        # xxx(okachaiev): hack
+        # proper support for array of distributions would be fancy
+        log_prob = log_prob.reshape(obs['obs'].shape[0], -1).sum(dim=-1)
+
+        return actions, values, log_prob
+
+    def evaluate_actions(self, obs, actions):
+        features = self.extract_features(obs)
+        latent_pi, latent_vf = self.mlp_extractor(features)
+
+        # xxx(okachaiev): hack to make multicategorical work
+        # (1, 16, 16, 78) -> (1*16*16, 78)
+        masks = obs['masks'].reshape(-1, self.action_space.nvec[1:].sum())
+        latent_pi = latent_pi.reshape(-1, self.action_space.nvec[1:].sum())
+        latent_pi = torch.where(masks.bool(), latent_pi, torch.tensor(-1e+8, device=self.device))
+
+        distribution = self._get_action_dist_from_latent(latent_pi)
+        actions = actions.reshape(-1, len(self.action_space.nvec[1:]))
+        log_prob = distribution.log_prob(actions)
+
+        # xxx(okachaiev): hack
+        log_prob = log_prob.reshape(obs['obs'].shape[0], -1).sum(dim=-1)
+
+        values = self.value_net(latent_vf)
+
+        # xxx(okachaiev): hack
+        # proper support for array of distributions would be fancy
+        return values, log_prob, distribution.entropy().reshape(obs['obs'].shape[0], -1).sum(dim=-1)
+
+
+if __name__ == "__main__":
+    register_policy('MicroRTSGridActorCritic', MicroRTSGridActorCritic)
+
+    envs = CustomMicroRTSGridMode(
+        num_selfplay_envs=0,
+        max_steps=2000,
+        render_theme=2,
+        ai2s=[
+            microrts_ai.randomBiasedAI,
+            # microrts_ai.lightRushAI,
+            # microrts_ai.workerRushAI,
+            # microrts_ai.coacAI,
+        ],
+        map_path="maps/16x16/basesWorkers16x16.xml",
+        reward_weight=np.array([10.0, 1.0, 1.0, 0.2, 1.0, 4.0])
+    )
+    envs = VecMonitor(envs)
+
+    model = PPO('MicroRTSGridActorCritic', envs, verbose=1)
+    # xxx(okachaiev): hack
+    model.rollout_buffer.action_dim = envs.height*envs.width*len(envs.action_space.nvec[1:])
+    model.rollout_buffer.reset()
+    model.learn(total_timesteps=1_000_000)
