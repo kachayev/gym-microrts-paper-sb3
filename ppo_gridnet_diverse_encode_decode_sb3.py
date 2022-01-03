ac86000c0668b13cb6146ec48dc4f5511fe43011
Oleksii Kachaiev <okachaiev@riotgames.com>
2022-01-02 19:45:11 -0500

    Simplified MicroRTSStatsRecorder code

--- ppo_gridnet_diverse_encode_decode_sb3.py
+++ ppo_gridnet_diverse_encode_decode_sb3.py
@@ -158,17 +158,17 @@ class MicroRTSStatsRecorder(VecEnvWrapper):
 
     def step_wait(self):
         obs, rews, dones, infos = self.venv.step_wait()
-        for i in range(len(dones)):
-            self.raw_rewards[i] += [infos[i]["raw_rewards"]]
+        for ind in range(len(dones)):
+            self.raw_rewards[ind].append(infos[ind]["raw_rewards"])
         newinfos = list(infos[:])
-        for i in range(len(dones)):
-            if dones[i]:
-                info = infos[i].copy()
-                raw_rewards = np.array(self.raw_rewards[i]).sum(0)
-                raw_names = [str(rf) for rf in self.rfs]
+        raw_names = [str(rf) for rf in self.rfs]
+        for ind, done in enumerate(dones):
+            if done:
+                info = infos[ind].copy()
+                raw_rewards = np.array(self.raw_rewards[ind]).sum(0)
                 info["microrts_stats"] = dict(zip(raw_names, raw_rewards))
-                self.raw_rewards[i] = []
-                newinfos[i] = info
+                self.raw_rewards[ind] = []
+                newinfos[ind] = info
         return obs, rews, dones, newinfos
 
 
