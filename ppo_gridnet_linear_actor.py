87060de83ae8889cbe7bf10faa8741a411b082a7
Oleksii Kachaiev <okachaiev@riotgames.com>
2022-01-08 15:02:57 -0800

    First naive pass

--- ppo_gridnet_linear_actor.py
+++ ppo_gridnet_linear_actor.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-from typing import Optional
 
 from stable_baselines3.common.utils import get_device
 from stable_baselines3.common.policies import register_policy
@@ -45,17 +44,38 @@ class Encoder(nn.Module):
         return x
 
 
-class MicroRTSExtractorLinearCritic(MicroRTSExtractor):
+class Actor(nn.Module):
+    
+    def __init__(self, output_channels, hidden_dim=32, num_cells=256):
+        super(Actor, self).__init__()
+
+        self.output_channels = output_channels
+        self.hidden_dim = hidden_dim
+        self.num_cells = num_cells
+        self.network = nn.Sequential(
+            nn.Linear(1, self.hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim, self.output_channels)
+        )
+
+    def forward(self, x):
+        x = self.network(x.unsqueeze(-1))
+        x = x.reshape((-1, self.output_channels*self.num_cells))
+        return x
+
+
+class MicroRTSExtractorLinearActor(MicroRTSExtractor):
 
     def __init__(
         self,
         input_channels: int = 27,
         output_channels: int = 78,
         encoder_norm_type: float = 2.,
-        action_space_size: Optional[int] = None,
+        action_space_size: int = 78*256,
+        actor_hidden_dim: int = 32,
         device: str = "auto"
     ):
-        super(MicroRTSExtractorLinearCritic, self).__init__()
+        super(MicroRTSExtractorLinearActor, self).__init__()
 
         self.latent_dim_pi = action_space_size
         self.latent_dim_vf = 256
@@ -63,40 +83,34 @@ class MicroRTSExtractorLinearCritic(MicroRTSExtractor):
         self.device = get_device(device)
 
         self.latent_net = Encoder(input_channels, norm_type=encoder_norm_type).to(self.device)
-
-        self.policy_net = nn.Sequential(
-            Reshape((-1, 256, 1, 1)),
-            layer_init(nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1)),
-            nn.ReLU(),
-            layer_init(nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1)),
-            nn.ReLU(),
-            layer_init(nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1)),
-            nn.ReLU(),
-            layer_init(nn.ConvTranspose2d(32, output_channels, 3, stride=2, padding=1, output_padding=1)),
-            Transpose((0, 2, 3, 1)),
-            Reshape((-1, action_space_size))
-        ).to(self.device)
-
+        self.policy_net = Actor(output_channels, hidden_dim=actor_hidden_dim).to(self.device)
         self.value_net = nn.Identity()
 
 
-class MicroRTSGridActorLinearCritic(MicroRTSGridActorCritic):
+class MicroRTSGridActorLinearActor(MicroRTSGridActorCritic):
 
     def _build_mlp_extractor(self) -> None:
-        self.mlp_extractor = MicroRTSExtractorLinearCritic(
+        self.mlp_extractor = MicroRTSExtractorLinearActor(
             input_channels=self.input_channels,
             output_channels=self.action_plane.sum(),
             action_space_size=self.action_space.nvec.sum(),
             encoder_norm_type=self.hparams['encoder_norm'],
+            actor_hidden_dim=self.hparams['actor_hidden_dim'],
         )
 
 
 if __name__ == "__main__":
-    register_policy('MicroRTSGridActorCritic', MicroRTSGridActorLinearCritic)
+    register_policy('MicroRTSGridActorCritic', MicroRTSGridActorLinearActor)
 
     parser = make_parser()
     parser.add_argument('--encoder-norm', type=float, default=0.,
                         help="Lp norm for CNN embeddings, setting to 0 skips normalization")
+    parser.add_argument('--actor-hidden-dim', type=int, default=32,
+                        help="Hidden layer for linear actor network")
     args = parse_arguments(parser)
 
-    main(args, policy_hparams=dict(encoder_norm=args.encoder_norm))
+    hparams = dict(
+        encoder_norm=args.encoder_norm,
+        actor_hidden_dim=args.actor_hidden_dim,
+    )
+    main(args, policy_hparams=hparams)
