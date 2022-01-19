ae6e50b453512f5d3bd3a9a0bb534ee7c0ee370c
Oleksii Kachaiev <okachaiev@riotgames.com>
2022-01-19 10:05:20 -0800

    Action ticks, hitpoint progress bar

--- rendering.py
+++ rendering.py
@@ -1,6 +1,7 @@
 import numpy as np
+import pyglet
 
-from gym.envs.classic_control.rendering import Viewer
+from gym.envs.classic_control.rendering import Viewer, Geom, Line
 
 viewer = Viewer(640, 640)
 offset = 20
@@ -15,17 +16,19 @@ pink = (204,121,167)
 orange = (213,94,0)
 silver = (192,192,192)
 slategray = (112,128,144)
+darkred = (114,0,0)
 
 batch = pyglet.graphics.Batch()
 background = pyglet.graphics.OrderedGroup(0)
 cicle_foreground = pyglet.graphics.OrderedGroup(1)
 texts = pyglet.graphics.OrderedGroup(2)
+progress_group = pyglet.graphics.OrderedGroup(3)
 
 def cell_center_coords(cell):
     row, col = cell
     return centers[col-1], 640-centers[row-1]
 
-def resource_label(resources, x, y, font_size=12):
+def resource_label_geom(resources, x, y, font_size=12):
     return pyglet.text.Label(
         str(resources),
         font_size=font_size,
@@ -37,13 +40,14 @@ def resource_label(resources, x, y, font_size=12):
     )
 
 def resource_geom(cell, resources):
+    if resources == 0: return None
     x, y = cell_center_coords(cell)
     rect = pyglet.shapes.Rectangle(
         x-step/2, y-step/2, step, step,
         color=green,
         batch=batch, group=background
     )
-    text = resource_label(resources, x, y)
+    text = resource_label_geom(resources, x, y)
     return (rect, text)
 
 def building_geom(cell, building_type, hitpoints, production, resources, owner):
@@ -56,15 +60,39 @@ def building_geom(cell, building_type, hitpoints, production, resources, owner):
         color=color, border_color=border_color,
         batch=batch, group=background
     )
-    text = resource_label(resources, x, y) if resources > 0 else None
+    text = resource_label_geom(resources, x, y)
     return rect, text
 
 unit_config = {
-    "worker": (silver, 0.6),
-    "light": (yellow, 0.7),
-    "heavy": (orange, 0.7),
+    "worker": (silver, 0.5),
+    "light": (yellow, 0.6),
+    "heavy": (orange, 0.6),
     "range": (deepskyblue, 0.8),
 }
+
+directions = {
+    "n": (0, 1),
+    "e": (1, 0),
+    "s": (0, -1),
+    "w": (-1, 0),
+}
+
+# xxx(okachaiev): would be nice to have geometry over point
+# with overloaded + and *
+def translate_direction(direction, length):
+    offset_x, offset_y = directions[direction]
+    return (offset_x*length, offset_y*length)
+
+def unit_tick_geom(cell_coors, radius, direction, color):
+    if direction is None: return None
+    x, y = cell_coors
+    offset_x, offset_y = translate_direction(direction, radius)
+    offset_x_hat, offset_y_hat = translate_direction(direction, step)
+    return pyglet.shapes.Line(
+        x+offset_x, y+offset_y, x+offset_x_hat, y+offset_y_hat,
+        width=2, color=color,
+        batch=batch, group=texts
+    )
 
 def unit_geom(cell, unit_type, hitpoints, direction, resources, owner):
     x, y = cell_center_coords(cell)
@@ -80,17 +108,35 @@ def unit_geom(cell, unit_type, hitpoints, direction, resources, owner):
         color=color,
         batch=batch, group=cicle_foreground
     )
-    text = resource_label(resources, x, y, font_size=9) if resources > 0 else None
-    return back_circle, circle, text
+    text = resource_label_geom(resources, x, y, font_size=9)
+    tick = unit_tick_geom((x,y), radius*step/2, direction, border_color)
+    hitpoints_progress = None
+    if hitpoints < 10:
+        # xxx(okachaiev): progress bar for units could look like missing segment
+        # rather than the bar on top of them
+        hitpoints_progress = progress_bar_geom((x,y), hitpoints/10, color, darkred)
+    return back_circle, circle, text, tick, hitpoints_progress
 
-def progress_bar(cell, progress, label_text, owner):
-    x, y = cell_center_coords(cell)
-    color = blue if owner == 1 else pink
-    bar = pyglet.shapes.Rectangle(
+def progress_bar_geom(cell_coords, progress, color, background_color):
+    x, y = cell_coords
+    left_bar = pyglet.shapes.Rectangle(
         x-step/2, y+step/2-(0.2*step), step*progress, 0.2*step,
         color=color,
-        batch=batch, group=background
+        batch=batch, group=progress_group
     )
+    right_bar = None
+    if background_color is not None:
+        right_bar = pyglet.shapes.Rectangle(
+            x-step/2+step*progress, y+step/2-0.2*step, step*(1-progress), 0.2*step,
+            color=background_color,
+            batch=batch, group=progress_group
+        )
+    return left_bar, right_bar
+
+def production_geom(cell, progress, label_text, owner):
+    x, y = cell_center_coords(cell)
+    color = blue if owner == 1 else pink
+    bar = progress_bar_geom((x,y), progress, color, None)
     text = pyglet.text.Label(
         label_text,
         font_size=8,
@@ -119,13 +165,13 @@ g5 = building_geom((3,3), "base", 3, None, 3, 1)
 g6 = building_geom((14,14), "base", 3, None, 7, 2)
 g7 = building_geom((3,7), "barracks", 3, None, 0, 1)
 
-g8 = unit_geom((3,8), "worker", 0, None, 1, 1)
-g9 = unit_geom((8,3), "light", 0, None, 0, 2)
-g10 = unit_geom((12,12), "heavy", 0, None, 3, 2)
-g11 = unit_geom((10,14), "range", 0, None, 0, 2)
+g8 = unit_geom((3,8), "worker", 20, None, 1, 1)
+g9 = unit_geom((8,3), "light", 20, "w", 0, 2)
+g10 = unit_geom((12,12), "heavy", 3, None, 3, 2)
+g11 = unit_geom((10,14), "range", 5, "n", 0, 2)
 
-g12 = progress_bar((9,9), 0.5, "worker", 1)
-g13 = progress_bar((11,9), 0.8, "worker", 2)
+g12 = production_geom((9,9), 0.5, "worker", 1)
+g13 = production_geom((11,9), 0.8, "worker", 2)
 
 viewer.add_geom(Proxy(batch))
 
@@ -135,5 +181,5 @@ for start in xs:
 
 viewer.render()
 
-while False:
+while True:
     viewer.window.dispatch_events()
